b6c7db939ee03e6790aa370fd8cd6fef2b2b12d2
shantanu darade <shantanudarade717@gmail.com>
2024-06-17 15:05:29 +0530

    default value

--- rectangle.py
+++ rectangle.py
@@ -1,10 +1,14 @@
-def perimeter(length, breadth):
+def perimeter_rectangle(length, breadth):
     p=2*(length+breadth)
     return p
  
- def area(length,breadth):
+ def area_rectangle(length,breadth):
     a=length*breadth
     return a
+    
+ l=int(input('Enter the length: ')
+ b=int(input('Enter the breadth: ')
  
- 
+ print(perimeter(l,b))
+ print(area(l,b))
  
